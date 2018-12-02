46457667305d7a0e2e91ccae66e9bd065890278e
Willson Mock <willson.mock@gmail.com>
2018-12-01 16:48:03 -0800

    Day 1 - complete part 1

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -1,3 +1,16 @@
 defmodule AdventOfCode2018.Day1 do
+  @input_path Path.join(File.cwd!(), "assets/day1_input.txt")
 
+  def main do
+    @input_path
+    |> process_file
+    |> Enum.sum
+  end
+
+  def process_file(file) do
+    file
+    |> File.read!()
+    |> String.split(~r/\n/)
+    |> Enum.map(&String.to_integer/1)
+  end
 end
