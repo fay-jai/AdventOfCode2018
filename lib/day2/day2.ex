d89621c249da78c0b01342cb0b28c4f09ef49a1b
Willson Mock <willson.mock@gmail.com>
2018-12-03 00:00:17 -0800

    Day 2 - complete part 2

--- lib/day2/day2.ex
+++ lib/day2/day2.ex
@@ -20,7 +20,7 @@ defmodule AdventOfCode2018.Day2 do
   What is the checksum for your list of box IDs?
   """
   def part1 do
-    data = @input_path |> process_file
+    data = @input_path |> process_file |> Enum.map(&produce_string_hash/1)
 
     num_two = data |> get_count(2)
     num_three = data |> get_count(3)
@@ -46,14 +46,18 @@ defmodule AdventOfCode2018.Day2 do
   What letters are common between the two correct box IDs? (In the example above, this is found by removing the differing character from either ID, producing fgij.)
   """
   def part2 do
+    [correct_box1, correct_box2] =
+      @input_path
+      |> process_file
+      |> get_correct_boxes
 
+    get_common_characters(correct_box1, correct_box2)
   end
 
   defp process_file(file) do
     file
     |> File.read!
     |> String.split(~r/\n/)
-    |> Enum.map(&produce_string_hash/1)
   end
 
   defp produce_string_hash(string) do
@@ -71,4 +75,29 @@ defmodule AdventOfCode2018.Day2 do
     |> Enum.filter(&(&1 == true))
     |> Enum.count
   end
+
+  defp get_correct_boxes([]), do: []
+  defp get_correct_boxes([first_box | rest_boxes]) do
+    result = rest_boxes |> Enum.filter(fn (box) -> are_correct_boxes(first_box, box) end)
+
+    if length(result) > 0 do
+      [ found_box ] = result
+      [ first_box, found_box ]
+    else
+      get_correct_boxes(rest_boxes)
+    end
+  end
+
+  defp are_correct_boxes(box1, box2), do: are_correct_boxes(box1, box2, 0)
+
+  defp are_correct_boxes("", "", num_off), do: num_off < 2
+  defp are_correct_boxes(_, _, num_off) when num_off > 2, do: false
+  defp are_correct_boxes(<< char1, rest1::binary >>, << char2, rest2::binary >>, num_off) when char1 == char2, do: are_correct_boxes(rest1, rest2, num_off)
+  defp are_correct_boxes(<< _, rest1::binary >>, << _, rest2::binary >>, num_off), do: are_correct_boxes(rest1, rest2, num_off + 1)
+
+  defp get_common_characters(box1, box2), do: get_common_characters(box1, box2, "")
+
+  defp get_common_characters("", "", result), do: result
+  defp get_common_characters(<< char1, rest1::binary >>, << char2, rest2::binary >>, result) when char1 == char2, do: get_common_characters(rest1, rest2, result <> << char1>>)
+  defp get_common_characters(<< _, rest1::binary >>, << _, rest2::binary >>, result), do: get_common_characters(rest1, rest2, result)
 end
