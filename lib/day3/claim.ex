1766f80a8240486185d6c7dae43088a1a7a682f3
Willson Mock <willson.mock@gmail.com>
2018-12-08 22:02:21 -0800

    Day 3 - create claim struct

---
+++ lib/day3/claim.ex
@@ -0,0 +1,8 @@
+defmodule AdventOfCode2018.Day3.Claim do
+  defstruct id: nil,
+            x_cord: 0,
+            y_cord: 0,
+            width: 1,
+            height: 1,
+            coords: []
+end
---
+++ lib/day3/day3.ex
@@ -0,0 +1,7 @@
+defmodule AdventOfCode2018.Day3 do
+  @input_path Path.join(File.cwd!(), "assets/day3_input.txt")
+
+  def part1() do
+
+  end
+end
