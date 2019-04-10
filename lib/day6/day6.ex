c0b5e0852905f41ecec4af5bfc020feb02cbd22e
Willson Mock <willson.mock@gmail.com>
2019-04-10 15:37:44 -0700

    Add bounding_coordinates

--- lib/day6/day6.ex
+++ lib/day6/day6.ex
@@ -2,7 +2,24 @@ defmodule AdventOfCode2018.Day6 do
   alias AdventOfCode2018.Helpers
 
   def part1() do
-    Helpers.read_file_and_parse(6)
+  end
+
+  def bounding_coordinates() do
+    coordinates_map = parse_input_into_coordinates_map()
+    coordinates_keys = get_coordinates_keys()
+    coordinates_values = get_coordinates_values()
+
+    left = coordinates_values |> min_coordinate("x")
+    right = coordinates_values |> max_coordinate("x")
+    top = coordinates_values |> min_coordinate("y")
+    bottom = coordinates_values |> max_coordinate("y")
+
+    %{
+      left: Enum.find(coordinates_keys, fn (key) -> Map.get(coordinates_map, key) == left end),
+      right: Enum.find(coordinates_keys, fn (key) -> Map.get(coordinates_map, key) == right end),
+      top: Enum.find(coordinates_keys, fn (key) -> Map.get(coordinates_map, key) == top end),
+      bottom: Enum.find(coordinates_keys, fn (key) -> Map.get(coordinates_map, key) == bottom end)
+    }
   end
 
   def get_closest_input_coordinate(coordinate) do
@@ -53,6 +70,26 @@ defmodule AdventOfCode2018.Day6 do
 
   def manhattan_distance({x_a, y_a}, {x_b, y_b}) do
     abs(x_a - x_b) + abs(y_a - y_b)
+  end
+
+  def min_coordinate(coordinates, "x") do
+    coordinates
+    |>  Enum.reduce(fn (coord, min_coord) ->
+          {x_coord, _} = coord
+          {min_x_coord, _} = min_coord
+
+          if x_coord < min_x_coord, do: coord, else: min_coord
+        end)
+  end
+
+  def min_coordinate(coordinates, "y") do
+    coordinates
+    |>  Enum.reduce(fn (coord, min_coord) ->
+          {_, y_coord} = coord
+          {_, min_y_coord} = min_coord
+
+          if y_coord < min_y_coord, do: coord, else: min_coord
+        end)
   end
 
   def max_coordinate(coordinates, "x") do
