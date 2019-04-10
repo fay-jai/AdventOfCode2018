8d7445e3dfebf24de3606426e3598df9e2372ed2
Willson Mock <willson.mock@gmail.com>
2019-04-10 13:45:58 -0700

    Add implementation for max_x_coordinate

--- lib/day6/day6.ex
+++ lib/day6/day6.ex
@@ -9,11 +9,31 @@ defmodule AdventOfCode2018.Day6 do
     Helpers.read_file_and_parse(6)
     |>  Enum.map(fn (string) ->
           [x, y] = String.split(string, ", ")
-          {x, y}
+          {String.to_integer(x), String.to_integer(y)}
         end)
   end
 
   def manhattan_distance({x_a, y_a}, {x_b, y_b}) do
     abs(x_a - x_b) + abs(y_a - y_b)
+  end
+
+  def max_coordinate(coordinates, "x") do
+    coordinates
+    |>  Enum.reduce(fn (coord, max_coord) ->
+          {x_coord, _} = coord
+          {max_x_coord, _} = max_coord
+
+          if x_coord > max_x_coord, do: coord, else: max_coord
+        end)
+  end
+
+  def max_coordinate(coordinates, "y") do
+    coordinates
+    |>  Enum.reduce(fn (coord, max_coord) ->
+          {_, y_coord} = coord
+          {_, max_y_coord} = max_coord
+
+          if y_coord > max_y_coord, do: coord, else: max_coord
+        end)
   end
 end
