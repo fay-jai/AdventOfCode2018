97e5a421db867b8e025dc7bc54cd3f8f032d9f16
Willson Mock <willson.mock@gmail.com>
2019-04-11 10:05:10 -0700

    Add setup to Day6 tests

--- test/day6_test.exs
+++ test/day6_test.exs
@@ -3,10 +3,25 @@ defmodule Day6Test do
 
   alias AdventOfCode2018.Day6
 
-  test "manhattan distance between 2 points" do
-      point_a = {194, 200}
-      point_b = {299, 244}
+  setup do
+    coordinates = """
+    8, 0
+    2, 1
+    7, 2
+    1, 3
+    3, 3
+    5, 5
+    3, 7
+    9, 8
+    0, 9
+    """
+    {:ok, coordinates: coordinates}
+  end
 
-      assert AdventOfCode2018.Day6.manhattan_distance(point_a, point_b) == (299 - 194) + (244 - 200)
+  test "manhattan distance between 2 points" do
+    point_a = {194, 200}
+    point_b = {299, 244}
+
+    assert AdventOfCode2018.Day6.manhattan_distance(point_a, point_b) == (299 - 194) + (244 - 200)
   end
 end
